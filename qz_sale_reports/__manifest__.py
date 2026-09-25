8077050070631e1d325dd069a01519782149049f
Bhumi Zinzuvadiya <bhumi@qzinfomatics.com>
2026-09-25 16:29:41 +0530

    [UPD] updated placeholder

--- qz_sale_reports/__manifest__.py
+++ qz_sale_reports/__manifest__.py
@@ -1,6 +1,6 @@
 {
     'name': 'QZ Sales Reports',
-    'version': '19.0.1.0.0',
+    'version': '19.0.1.0.1',
     'category': 'Sales/Sales',
     'summary': 'QZ sales proposal with quotation field mapping',
     'author': 'Quiz Infomatics',
